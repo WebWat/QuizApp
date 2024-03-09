d0d9dcc71a96afcb22b7d120b2ec616630598f7a
Serega <64882377+WebWat@users.noreply.github.com>
2024-03-09 13:55:46 +0300

    Изменил форму создания ответов к вопросам

--- Web/models.py
+++ Web/models.py
@@ -15,7 +15,7 @@ class Question(models.Model):
     #explanation = models.CharField(max_length = 2000, default = "")
 
 class SingleChoice(models.Model):
-    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    question = models.OneToOneField(Question, on_delete = models.CASCADE, primary_key = True)
     correct_answer = models.IntegerField(default = 0)
 
 class SingleChoiceAnswers(models.Model):
@@ -23,11 +23,11 @@ class SingleChoiceAnswers(models.Model):
     text = models.CharField(max_length = 100)
     
 class MultipleChoice(models.Model):
-    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    question = models.OneToOneField(Question, on_delete = models.CASCADE, primary_key = True)
 
 class MultipleChoiceAnswers(models.Model):
     multiple_choice = models.ForeignKey(MultipleChoice, on_delete = models.CASCADE)
     text = models.CharField(max_length = 100)
-    is_correct = models.BooleanField()
+    is_correct = models.BooleanField(default = False)
     
     
--- Web/views.py
+++ Web/views.py
@@ -1,8 +1,20 @@
 from django.http import HttpResponseNotFound
 from django.contrib.auth.decorators import login_required
 import datetime
-from .models import Test, Question, SingleChoice, SingleChoiceAnswers, MultipleChoice, MultipleChoiceAnswers
-from .forms import TestForm, QuestionForm, SingleAnswerForm, MultipleAnswerForm
+from .models import (
+    Test,
+    Question, 
+    SingleChoice, 
+    SingleChoiceAnswers, 
+    MultipleChoice, 
+    MultipleChoiceAnswers
+)
+from .forms import (
+    TestForm, 
+    QuestionForm, 
+    AnswerForm, 
+    EditQuestionForm
+)
 from django.shortcuts import redirect, render
 
 def index(request):
@@ -53,6 +65,15 @@ def delete_test(request, id):
     except Test.DoesNotExist:
         return HttpResponseNotFound("<h2>Тест не найден</h2>")
     
+@login_required    
+def publish_test(request, id):
+    try:
+        test = Test.objects.get(user_id = request.user.id, id = id)
+        test.is_published = True
+        test.save()
+    except Test.DoesNotExist:
+        return HttpResponseNotFound("<h2>Тест не найден</h2>")
+    
 @login_required       
 def questions(request, test_id):
     try:
@@ -62,7 +83,7 @@ def questions(request, test_id):
         return render(request, "questions.html", context = data)
     except Test.DoesNotExist:
         return HttpResponseNotFound("<h2>Тест не найден</h2>")
-    
+
 @login_required
 def create_question(request, test_id):
     try:
@@ -73,9 +94,9 @@ def create_question(request, test_id):
                 question = test.question_set.create(issue = form.cleaned_data["issue"], 
                                                     choice_type = form.cleaned_data["choice_type"])
                 if question.choice_type == "0":
-                    question.singlechoice_set.create()
+                    SingleChoice.objects.create(question = question)
                 else:
-                    question.multiplechoice_set.create()
+                    MultipleChoice.objects.create(question = question)
                 return redirect(f"/questions/{test_id}/")
         else:
             form = QuestionForm()
@@ -89,15 +110,13 @@ def edit_question(request, test_id, id):
         test = Test.objects.get(user_id = request.user.id, id = test_id)
         question = Question.objects.get(test_id = test.id, id = id)
         if request.method == "POST":
-            form = QuestionForm(request.POST)
+            form = EditQuestionForm(request.POST)
             if form.is_valid():
                 question.issue = form.cleaned_data["issue"]
-                #TODO: добавить проверку ответов
-                #question.choice_type = form.cleaned_data["choice_type"]
                 question.save()
                 return redirect(f"/questions/{test_id}/")
         else:
-            form = QuestionForm(initial = { "issue": question.issue, "choice_type": question.choice_type })
+            form = EditQuestionForm(initial = { "issue": question.issue })
         return render(request, "edit_question.html", { "form": form })
     except (Test.DoesNotExist, Question.DoesNotExist):
         return HttpResponseNotFound("<h2>Вопрос не найден</h2>")
@@ -113,58 +132,50 @@ def delete_question(request, test_id, id):
         return HttpResponseNotFound("<h2>Вопрос не найден</h2>")
 
 @login_required       
-def answers(request, test_id, question_id):
+def set_true(request, test_id, question_id, id):
     try:
         test = Test.objects.get(user_id = request.user.id, id = test_id)
         question = Question.objects.get(test_id = test.id, id = question_id)
         if question.choice_type == 0:
-            single_choice = SingleChoice.objects.get(question_id = question.id)
-            answers = SingleChoiceAnswers.objects.filter(single_choice_id = single_choice.id)
-            if request.method == "POST":
-                correct_id = request.POST.get("correct_id")
-                if answers.filter(id = correct_id).count() > 0:
-                    single_choice.correct_answer = correct_id
-                    single_choice.save()
-            data = { "test": test, 
-                     "question": question,
-                     "correct_answer": single_choice.correct_answer, 
-                     "answers": answers }
+            choice = SingleChoice.objects.get(question_id = question.id)
+            answers = SingleChoiceAnswers.objects.filter(single_choice_id = question.id)
+            if answers.filter(id = id).count() > 0:
+                choice.correct_answer = id
+                choice.save()
         else:
-            multiple_choice = MultipleChoice.objects.get(question_id = question.id)
-            data = { "test": test, 
-                     "question": question,
-                     "answers": MultipleChoiceAnswers.objects.filter(multiple_choice_id = multiple_choice.id) }
-        return render(request, "answers.html", context = data)
-    except (Test.DoesNotExist, Question.DoesNotExist, SingleChoice.DoesNotExist, MultipleChoice.DoesNotExist):
+            answer = MultipleChoiceAnswers.objects.get(id = id, multiple_choice_id = question.id)
+            answer.is_correct = not answer.is_correct
+            answer.save()
+        return redirect(f"/questions/{test_id}/")
+    except (Test.DoesNotExist, 
+            Question.DoesNotExist, 
+            SingleChoice.DoesNotExist, 
+            SingleChoiceAnswers.DoesNotExist,
+            MultipleChoiceAnswers.DoesNotExist):
         return HttpResponseNotFound("<h2>Вопрос не найден</h2>")
 
-#TODO: Разделить маршрутизацию для разных типов вопроса???  
 @login_required
 def create_answer(request, test_id, question_id):
     try:
         test = Test.objects.get(user_id = request.user.id, id = test_id)
         question = Question.objects.get(test_id = test.id, id = question_id)
         if request.method == "POST":
-            if question.choice_type == 0:
-                form = SingleAnswerForm(request.POST)
-            else:
-                form = MultipleAnswerForm(request.POST)
+            form = AnswerForm(request.POST)
             if form.is_valid():
                 if question.choice_type == 0:
                     single_choice = SingleChoice.objects.get(question_id = question.id)
                     single_choice.singlechoiceanswers_set.create(text = form.cleaned_data["text"])
                 else:
                     multiple_choice = MultipleChoice.objects.get(question_id = question.id)
-                    multiple_choice.multiplechoiceanswers_set.create(text = form.cleaned_data["text"],
-                                                                     is_correct = form.cleaned_data["is_correct"])
-                return redirect(f"/answers/{test_id}/{question_id}/")
+                    multiple_choice.multiplechoiceanswers_set.create(text = form.cleaned_data["text"])
+                return redirect(f"/questions/{test_id}/")
         else:
-            if question.choice_type == 0:
-                form = SingleAnswerForm()
-            else:
-                form = MultipleAnswerForm()
+            form = AnswerForm()
         return render(request, "create_answer.html", { "form": form })
-    except (Test.DoesNotExist, Question.DoesNotExist, SingleChoice.DoesNotExist, MultipleChoice.DoesNotExist):
+    except (Test.DoesNotExist, 
+            Question.DoesNotExist, 
+            SingleChoice.DoesNotExist, 
+            MultipleChoice.DoesNotExist):
         return HttpResponseNotFound("<h2>Ответ не найден</h2>")
 
 @login_required
@@ -173,32 +184,20 @@ def edit_answer(request, test_id, question_id, id):
         test = Test.objects.get(user_id = request.user.id, id = test_id)
         question = Question.objects.get(test_id = test.id, id = question_id)
         if question.choice_type == 0:
-            choice = SingleChoice.objects.get(question_id = question.id)
-            answer = SingleChoiceAnswers.objects.get(single_choice_id = choice.id, id = id)
+            answer = SingleChoiceAnswers.objects.get(single_choice_id = question_id, id = id)
         else:
-            choice = MultipleChoice.objects.get(question_id = question.id)
-            answer = MultipleChoiceAnswers.objects.get(multiple_choice_id = choice.id, id = id)
+            answer = MultipleChoiceAnswers.objects.get(multiple_choice_id = question_id, id = id)
         if request.method == "POST":
-            if question.choice_type == 0:
-                form = SingleAnswerForm(request.POST)
-            else:
-                form = MultipleAnswerForm(request.POST)
+            form = AnswerForm(request.POST)
             if form.is_valid():
                 answer.text = form.cleaned_data["text"]
-                if question.choice_type == 1:
-                    answer.is_correct = form.cleaned_data["is_correct"]
                 answer.save()
-                return redirect(f"/answers/{test_id}/{question_id}/")
+                return redirect(f"/questions/{test_id}/")
         else:
-            if question.choice_type == 0:
-                form = SingleAnswerForm(initial = { "text": answer.text })
-            else:
-                form = MultipleAnswerForm(initial = { "text": answer.text, "is_correct": int(answer.is_correct) })
+            form = AnswerForm(initial = { "text": answer.text })
         return render(request, "edit_answer.html", { "form": form })
     except (Test.DoesNotExist, 
-            Question.DoesNotExist, 
-            SingleChoice.DoesNotExist, 
-            MultipleChoice.DoesNotExist,
+            Question.DoesNotExist,
             SingleChoiceAnswers.DoesNotExist,
             MultipleChoiceAnswers.DoesNotExist):
         return HttpResponseNotFound("<h2>Ответ не найден</h2>")
@@ -209,17 +208,14 @@ def delete_answer(request, test_id, question_id, id):
         test = Test.objects.get(user_id = request.user.id, id = test_id)
         question = Question.objects.get(test_id = test.id, id = question_id)
         if question.choice_type == 0:
-            choice = SingleChoice.objects.get(question_id = question.id)
-            answer = SingleChoiceAnswers.objects.get(single_choice_id = choice.id, id = id)
+            answer = SingleChoiceAnswers.objects.get(single_choice_id = question_id, id = id)
         else:
-            choice = MultipleChoice.objects.get(question_id = question.id)
-            answer = MultipleChoiceAnswers.objects.get(multiple_choice_id = choice.id, id = id)
+            answer = MultipleChoiceAnswers.objects.get(multiple_choice_id = question_id, id = id)
         answer.delete()
-        return redirect(f"/answers/{test_id}/{question_id}/")
+        return redirect(f"/questions/{test_id}/")
     except (Test.DoesNotExist, 
             Question.DoesNotExist, 
-            SingleChoice.DoesNotExist, 
-            MultipleChoice.DoesNotExist,
             SingleChoiceAnswers.DoesNotExist,
             MultipleChoiceAnswers.DoesNotExist):
         return HttpResponseNotFound("<h2>Ответ не найден</h2>")
+
