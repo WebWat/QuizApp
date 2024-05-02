7defee8a1e336d8becc7920e7a07cb9c068ac685
Sergey <64882377+WebWat@users.noreply.github.com>
2024-05-02 11:05:08 +0300

    Правка

--- Main/urls.py
+++ Main/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 from Auth import views as auth
 from Web import views as web
@@ -24,8 +23,6 @@ from django.conf import settings
 from django.conf.urls.static import static
  
 urlpatterns = [
-     path("admin", admin.site.urls),
-
     # Auth
     path("login", auth.login),
     path("register", auth.register),
